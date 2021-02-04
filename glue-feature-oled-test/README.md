406d4779ebf6224aa6c4942dced132c57798a0e4
Hannes Rüger <hannesrueger@gmx.de>
2021-02-04 18:58:59 +0100

    added oled test feature

---
+++ glue-feature-oled-test/README.md
@@ -0,0 +1,19 @@
+# OLED Test Feature
+> This is a simple feature to test the OLED display.
+
+## Features:
+- Here you can
+- list everything
+- which your Feature
+- can do!
+
+## Installation
+ToDo
+
+## Development
+ToDo
+
+## License
+MIT
+
+*This Feature was created using the [Glue Feature Template](https://github.com/makeproaudio/glue-feature-template).*
---
+++ glue-feature-oled-test/src/index.ts
@@ -0,0 +1,98 @@
+import { Parameter, setSynapsesManager, NumberParameter } from "@makeproaudio/parameters-js";
+import { v4 } from "uuid";
+import { Feature, HWWidgetType, ZoneConfig } from "@makeproaudio/glue-feature-tools";
+import { EventEmitter } from "events";
+
+export default class OLEDTestFeature extends EventEmitter implements Feature {
+    public readonly zones: ZoneConfig[] = [
+        {
+            color: "#34eba4",
+            id: "slow",
+            name: "Toggle Slow",
+            description: "",
+            widgetTypes: [HWWidgetType.LEDBUTTON],
+        },
+        {
+            color: "#eb3471",
+            id: "fast",
+            name: "Toggle Fast",
+            description: "",
+            widgetTypes: [HWWidgetType.LEDBUTTON],
+        }
+    ];
+    private registry: any;
+    fastToggleParams: Map<number, Parameter<any>>;
+    slowToggleParams: Map<number, Parameter<any>>;
+    widgets: any;
+    layout = 4;
+
+
+    public constructor(settings: any, registry, synapsesManager: any) {
+        super();
+        setSynapsesManager(synapsesManager);
+        this.registry = registry;
+        this.fastToggleParams = new Map<number, Parameter<any>>();
+        this.slowToggleParams = new Map<number, Parameter<any>>();
+        for (let i = 0; i < 40; i++) {
+            const p = new NumberParameter(50, 0, 100, 1, v4(), (evt) => {
+                this.toggleLayoutFast();
+                p.color = "#ffffff";
+                setTimeout(() => {
+                    p.color = "#eb3471";
+                }, 1000)
+            });
+            this.fastToggleParams.set(i, p);
+        }
+        for (let i = 0; i < 40; i++) {
+            const p = new NumberParameter(50, 0, 100, 1, v4(), (evt) => {
+                this.toggleLayoutSlow();
+                p.color = "#ffffff";
+                setTimeout(() => {
+                    p.color = "#34eba4";
+                }, 1000)
+            });
+            this.slowToggleParams.set(i, p);
+        }
+
+        registry.registerInterest(null, (e, r) => {
+            if (e == "NEW_MATCH") {
+                for (const obj of r) {
+                    const tile = obj.objectReference;
+                    console.log('found tile');
+                    this.widgets = tile.widgets;
+                }
+            }
+        }, "tile", {
+            tileType: "4O",
+        });
+    }
+
+    giveParametersForZone(zoneConfig: ZoneConfig): Map<number, Parameter<any>> {
+        if (zoneConfig.id == "slow") {
+            return this.slowToggleParams;
+        }
+        return this.fastToggleParams;
+    }
+
+    private toggleLayoutFast() {
+        this.layout++;
+        if (this.layout > 4) {
+            this.layout = 1;
+        }
+        this.widgets[0].layout = this.layout;
+        this.widgets[1].layout = this.layout;
+        this.widgets[2].layout = this.layout;
+        this.widgets[3].layout = this.layout;
+    }
+
+    private toggleLayoutSlow() {
+        this.layout++;
+        if (this.layout > 4) {
+            this.layout = 1;
+        }
+        setTimeout(() => (this.widgets[0].layout = this.layout), 0)
+        setTimeout(() => (this.widgets[1].layout = this.layout), 1000)
+        setTimeout(() => (this.widgets[2].layout = this.layout), 2000)
+        setTimeout(() => (this.widgets[3].layout = this.layout), 3000)
+    }
+}
